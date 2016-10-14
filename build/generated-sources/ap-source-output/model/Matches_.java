b584dbd188b2ba7704fc406bb178a2c653f0dc17
asaf <smartush78@gmail.com>
2016-10-14 14:08:11 +0300

    still initialinzg db from socket server (shouuld be implemented elsewhere)

---
+++ build/generated-sources/ap-source-output/model/Matches_.java
@@ -0,0 +1,25 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-14T14:07:05")
+@StaticMetamodel(Matches.class)
+public class Matches_ { 
+
+    public static volatile SingularAttribute<Matches, String> tournamentId;
+    public static volatile SingularAttribute<Matches, Integer> roundNumber;
+    public static volatile SingularAttribute<Matches, String> status;
+    public static volatile SingularAttribute<Matches, String> opponents;
+    public static volatile SingularAttribute<Matches, Integer> number;
+    public static volatile SingularAttribute<Matches, String> type;
+    public static volatile SingularAttribute<Matches, Integer> groupNumber;
+    public static volatile SingularAttribute<Matches, String> date;
+    public static volatile SingularAttribute<Matches, String> id;
+    public static volatile SingularAttribute<Matches, String> timezone;
+    public static volatile SingularAttribute<Matches, String> matchFormat;
+    public static volatile SingularAttribute<Matches, Integer> stageNumber;
+    public static volatile SingularAttribute<Matches, String> discipline;
+
+}
