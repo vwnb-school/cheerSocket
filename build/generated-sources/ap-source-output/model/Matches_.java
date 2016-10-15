1d660bd9293d543c51551ecc2e6f281dc7db0324
megakoresh <megakoresh@gmail.com>
2016-10-16 01:01:48 +0300

    Just some files to get it to compile to WildFly correctly. If you still wanna use glassfish, change the server in project properties and netbeans will take care of the rest.

--- build/generated-sources/ap-source-output/model/Matches_.java
+++ build/generated-sources/ap-source-output/model/Matches_.java
@@ -4,22 +4,22 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-14T14:58:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-16T00:41:10")
 @StaticMetamodel(Matches.class)
 public class Matches_ { 
 
-    public static volatile SingularAttribute<Matches, String> tournamentId;
-    public static volatile SingularAttribute<Matches, Integer> roundNumber;
-    public static volatile SingularAttribute<Matches, String> status;
+    public static volatile SingularAttribute<Matches, String> date;
+    public static volatile SingularAttribute<Matches, String> timezone;
     public static volatile SingularAttribute<Matches, String> opponents;
-    public static volatile SingularAttribute<Matches, Integer> number;
+    public static volatile SingularAttribute<Matches, String> discipline;
+    public static volatile SingularAttribute<Matches, String> matchFormat;
     public static volatile SingularAttribute<Matches, String> type;
     public static volatile SingularAttribute<Matches, Integer> groupNumber;
-    public static volatile SingularAttribute<Matches, String> date;
-    public static volatile SingularAttribute<Matches, String> id;
-    public static volatile SingularAttribute<Matches, String> timezone;
-    public static volatile SingularAttribute<Matches, String> matchFormat;
+    public static volatile SingularAttribute<Matches, Integer> number;
+    public static volatile SingularAttribute<Matches, Integer> roundNumber;
+    public static volatile SingularAttribute<Matches, String> tournamentId;
     public static volatile SingularAttribute<Matches, Integer> stageNumber;
-    public static volatile SingularAttribute<Matches, String> discipline;
+    public static volatile SingularAttribute<Matches, String> id;
+    public static volatile SingularAttribute<Matches, String> status;
 
 }
